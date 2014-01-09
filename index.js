09f7e86b37c583b7c08fab855190a75ffed26679
Aseem Kishore <aseem.kishore@gmail.com>
2014-01-09 16:45:05 -0500

    Minor: don't leak local variable.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = express = require('express');
 
 // we add to Express's prototype, with back-compat for express 2.
 // (TODO this only patches HTTP servers in express 2, not HTTPS ones.)
-app = express.application || express.HTTPServer.prototype;
+var app = express.application || express.HTTPServer.prototype;
 
 // TODO support passing through parameters, e.g. ':id' etc.
 app.alias = function (from, to, status) {
